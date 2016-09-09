92b7bbd95b26b0dbc43a6d5b6b7d62df349680f4
Mick Chen <af.chen1983@gmail.com>
2016-09-08 17:44:55 -0700

    Initial Commit

---
+++ udpClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  udpClient
+//
+//  Created by Mick on 2016/9/8.
+//  Copyright © 2016年 ChengYang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
